696b1fe971d1089f47d93b956a99345baba6ab94
frdhcv <faridhv263@gmail.com>
2024-01-22 20:49:10 +0400

    Math App

--- src/main/java/lesson06/MathApp.java
+++ src/main/java/lesson06/MathApp.java
@@ -1,14 +1,10 @@
 package lesson06;
 
-import java.util.Scanner;
-
 public class MathApp {
     public static void main(String[] args) {
-        Scanner input = new Scanner(System.in);
-        System.out.println("Enter the number 1: ");
-        int a = input.nextInt();
-        System.out.println("Enter the number 2: ");
-        int b = input.nextInt();
-        System.out.println("The answer is: " + a % b);
+        for (int i = 0; i < 11; i++) {
+            System.out.println(i * 2);
+        }
     }
 }
+
