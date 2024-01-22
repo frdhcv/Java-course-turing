7f86c750f052426a5817e6ac9b5b5d8ba5384890
frdhcv <faridhv263@gmail.com>
2024-01-22 10:57:42 +0400

    season app

--- src/main/java/lesson05/ScannerApp.java
+++ src/main/java/lesson05/ScannerApp.java
@@ -6,6 +6,7 @@ import java.util.Scanner;
 public class ScannerApp {
     public static void main(String[] args) {
         Scanner input=new Scanner(System.in);
+        System.out.println("Enter the number of month");
         int a= input.nextInt();
         System.out.printf(a>0&&a<4?  "Winter":a>3&&a<7? "Spring":a>6&&a<10?"Summer":a>9&&a<13?"Fall":"false");
     }
