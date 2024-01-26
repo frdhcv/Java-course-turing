5bcdad789ed872ba443ceb87eed4b5a69526e0a9
frdhcv <faridhv263@gmail.com>
2024-01-26 15:02:37 +0400

    Eolymp Tasks

--- src/main/java/eolymp/tasks/ExactlyOneConditionOutofTwoApp.java
+++ src/main/java/eolymp/tasks/ExactlyOneConditionOutofTwoApp.java
@@ -6,7 +6,9 @@ public class ExactlyOneConditionOutofTwoApp {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         int a = scanner.nextInt();
-        if (a % 2 == 0 && (a < 0 && a % 3 == 0)) System.out.println("YES");
+        if (a % 2 == 0 && (a < 0 && a % 3 == 0)) System.out.println("NO");
+        else if (a % 2 == 0) System.out.println("YES");
+        else if (a < 0 && a % 3 == 0) System.out.println("YES");
         else System.out.println("NO");
     }
 }
