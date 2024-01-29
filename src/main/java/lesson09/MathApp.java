6141c38a2748f5d5eb30eeeee50b0e53816e9476
frdhcv <faridhv263@gmail.com>
2024-01-29 19:37:39 +0400

    Area of circle & square

--- src/main/java/lesson09/MathApp.java
+++ src/main/java/lesson09/MathApp.java
@@ -9,7 +9,7 @@ public class MathApp {
         int r = scanner.nextInt();
         System.out.println("Perimeter of square is: " + 4 * a);
         System.out.println("Area of square is: " + 2 * a);
-        System.out.println("Length of circle is: " + Math.PI * r * r);
-        System.out.println("Area of the+ circle is :" + Math.PI * r * r + " CM");
+        System.out.println("Length of circle is: " + Math.PI * Math.pow(a,2));
+        System.out.println("Area of the+ circle is :" + Math.PI * Math.pow(a,2) + " CM");
     }
 }
