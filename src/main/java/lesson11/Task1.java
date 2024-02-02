48fc041eb56da75c324beed46a9c718c74bb2d41
frdhcv <faridhv263@gmail.com>
2024-02-02 19:58:04 +0400

    Name &length

--- src/main/java/lesson11/Task1.java
+++ src/main/java/lesson11/Task1.java
@@ -3,17 +3,19 @@ package lesson11;
 import java.util.Scanner;
 
 public class Task1 {
-    public static void main(String[] args)  {
+    public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        String a= scanner.nextLine();
+        String a = scanner.nextLine();
         System.out.println(getName(a));
         System.out.println(Length(getName(a)));
 
     }
-    public static String getName(String name)
-    {
-        return name.substring(0,name.indexOf("@"));
-    }public static int Length(String namelength){
+
+    public static String getName(String name) {
+        return name.substring(0, name.indexOf("@"));
+    }
+
+    public static int Length(String namelength) {
         return namelength.length();
     }
 }
