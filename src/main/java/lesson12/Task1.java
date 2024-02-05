5c449ae97e03ed7574a37e2e2a17cf16c4c2185f
frdhcv <faridhv263@gmail.com>
2024-02-05 20:32:53 +0400

    Reverse word

--- src/main/java/lesson12/Task1.java
+++ src/main/java/lesson12/Task1.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 public class Task1 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        String result = reverseWord(scanner.next());
+        String result = reverseWord(scanner.nextLine());
         System.out.print(result);
     }
 
