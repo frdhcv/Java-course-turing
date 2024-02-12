ee76fa1bbdcbf00e8d37d5893cb634ad61a59285
frdhcv <faridhv263@gmail.com>
2024-02-12 20:12:56 +0400

    GradeApp

---
+++ src/main/java/Modul1/Task.java
@@ -0,0 +1,19 @@
+package Modul1;
+
+public class Task {
+    public static void main(String[] args) {
+        String[] name = {"Farid", "Vusal", "Ismayil", "Osman"};
+        int[] grade = {20, 15, 8, 9};
+        int[] age = {18, 19, 21, 27};
+        for (int i = 0; i < grade.length; i++) {
+            int max = 0;
+            if (max < grade[i]) {
+                System.out.println(grade[i]);
+                System.out.println(name[i]);
+                System.out.println(age[i]);
+            }
+            break;
+
+        }
+    }
+}
