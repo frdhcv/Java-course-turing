51577faa0f6aeb5b254d096596b9cdce9a604ea8
frdhcv <faridhv263@gmail.com>
2024-03-11 20:22:10 +0400

    Divide App

--- src/main/java/az/edu/turing/Modul1/lesson13/Task.java
+++ src/main/java/az/edu/turing/Modul1/lesson13/Task.java
@@ -5,10 +5,10 @@ import java.util.Scanner;
 public class Task {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        String n1= scanner.next();
-        String n2= scanner.next();
-        int a1=Integer.parseInt(n1);
-        int a2=Integer.parseInt(n2);
-        System.out.println(a1/a2);
+        String n1 = scanner.next();
+        String n2 = scanner.next();
+        int a1 = Integer.parseInt(n1);
+        int a2 = Integer.parseInt(n2);
+        System.out.println(a1 / a2);
     }
 }
